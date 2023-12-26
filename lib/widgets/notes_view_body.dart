f8e21a44f42a0e70eaeed25becd32354ec5979ea
FakhrElddin <fa1641@fayoum.edu.eg>
2023-12-26 02:01:01 +0200

    create note item and add fonts

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -16,10 +17,17 @@ class NotesViewBody extends StatelessWidget {
             height: 8,
           ),
           CustomAppBar(),
+          SizedBox(
+            height: 16,
+          ),
+          NoteItem(),
         ],
       ),
     );
   }
 }
+
+
+
 
 
