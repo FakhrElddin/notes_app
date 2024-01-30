19a85b2c623b2a468a0391158500c5b4bc5186f4
FakhrElddin <fa1641@fayoum.edu.eg>
2024-01-30 20:36:48 +0200

    colors list view part 2

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActive, required this.color});
+
+  final bool isActive;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
+    return isActive ? CircleAvatar(
+      radius: 38,
+      backgroundColor: Colors.white,
+      child: CircleAvatar(
+        backgroundColor: color,
+        radius: 34,
+      ),
+    ) 
+        : CircleAvatar(
+      backgroundColor: color,
       radius: 38,
     );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xff008BF8),
+    Color(0xffE5D352),
+    Color(0xff04E762),
+    Color(0xffF5B700),
+    Color(0xffDC0073),
+    Color(0xff89FC00),
+    Color(0xffFC1717),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +49,20 @@ class ColorsListView extends StatelessWidget {
       child: ListView.separated(
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => const ColorItem(),
-        itemCount: 10,
-        separatorBuilder: (context, index) => const SizedBox(width: 18,),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: (){
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              color: colors[index],
+              isActive: currentIndex == index,
+            ),
+          );
+        },
+        itemCount: colors.length,
+        separatorBuilder: (context, index) => const SizedBox(width: 8),
       ),
     );
   }
